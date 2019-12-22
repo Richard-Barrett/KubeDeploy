3019fa94ce67db5370e08b434e32a282a7c03814
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2019-12-21 19:22:26 -0600

    Update Install-MasterKube-Centos.sh

--- Installation/Linux/Flavors/Centos/Master/Install-MasterKube-Centos.sh
+++ Installation/Linux/Flavors/Centos/Master/Install-MasterKube-Centos.sh
@@ -44,6 +44,15 @@ EOF
 
 sudo sysctl --system
 
+# Configure Firewalld
+sudo firewall-cmd --permanent --add-port=6443/tcp
+sudo firewall-cmd --permanent --add-port=2379-2380/tcp
+sudo firewall-cmd --permanent --add-port=10250/tcp
+sudo firewall-cmd --permanent --add-port=10251/tcp
+sudo firewall-cmd --permanent --add-port=10252/tcp
+sudo firewall-cmd --permanent --add-port=10255/tcp
+sudo firewall-cmd –-reload
+
 # Initialize the Kube Master (MASTER ONLY)
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p $HOME/.kube
