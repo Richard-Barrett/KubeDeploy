77d8d2c4e139f414f1d0522f3f784509fa69e2e0
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2019-12-21 19:40:51 -0600

    Update InstallHelm-Ubuntu1804.sh

--- Helm/Installation/Linux/Flavors/Ubuntu/InstallHelm-Ubuntu1804.sh
+++ Helm/Installation/Linux/Flavors/Ubuntu/InstallHelm-Ubuntu1804.sh
@@ -5,3 +5,8 @@ curl -L https://git.io/get_helm.sh | bash
 
 # Create Resources
 kubectl apply -f ~/KubeDEploy/Helm/helm-rbac.yaml
+
+# For Specific Helm-Tiller Version
+# helm init --service-account=tiller --tiller-image=gcr.io/kubernetes-helm/tiller:v2.14.1   --history-max 300
+# Initialize Helm: Deploy Tiller
+helm init --service-account=tiller --history-max 300
