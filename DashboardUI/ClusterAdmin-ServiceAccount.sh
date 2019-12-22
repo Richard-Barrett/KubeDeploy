ad13c3559ac443c4bc73baee934980bb7c60e227
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2019-12-21 19:54:51 -0600

    Create ClusterAdmin-ServiceAccount.sh

---
+++ DashboardUI/ClusterAdmin-ServiceAccount.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# You can create a service account with cluster-admin role that will have access to all your resources
+kubectl create serviceaccount cluster-admin-dashboard-sa
+kubectl create clusterrolebinding cluster-admin-dashboard-sa \
+  --clusterrole=cluster-admin \
+  --serviceaccount=default:cluster-admin-dashboard-sa
+  
+# Copy the token from the generated secret
+kubectl get secret | grep cluster-admin-dashboard-sa >> admin_generated.txt
+
+# Redirect the Admin Token into admin_token.txt
+kubectl describe secret cluster-admin-dashboard-sa-token-6xm8l >> admin_token.txt
+
+# Create limited resources access service account
+kubectl create serviceaccount pod-viewer
+kubectl apply -f ~/KubeDeploy/DashboardUI/Resources/pod-viewer-role.yaml
+kubectl create clusterrolebinding pod-viewer-sa \
+  --clusterrole=pod-viewer \
+  --serviceaccount=default:pod-viewer-sa
