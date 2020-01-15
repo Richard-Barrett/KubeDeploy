4e08d389e3445aae763ffcb00ad67bbbc94e80fa
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-01-15 09:22:09 -0600

    Update Install-Workers-Ubuntu1804.sh

--- Installation/Linux/Flavors/Ubuntu/18.04/Workers/Install-Workers-Ubuntu1804.sh
+++ Installation/Linux/Flavors/Ubuntu/18.04/Workers/Install-Workers-Ubuntu1804.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Turn off Swap
+sudo swapoff –a
+
+# If then Statement to look and see if Curl is Installed
+sudo apt-get install curl
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
@@ -16,10 +22,3 @@ sudo apt-mark hold docker-ce kubelet kubeadm kubectl
 
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
-
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
