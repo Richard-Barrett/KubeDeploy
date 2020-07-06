ada4c39e65d93a2ba2fe712ae9eac33714f62b2e
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-07-06 10:17:26 -0500

    Create etcd_restore.sh

---
+++ Scripts/etcd_restore.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Restore the Cluster
+ETCDCTL_API=3 etcdctl --endpoints=https://[127.0.0.1]:2379 --cacert=/etc/kubernetes/pki/etcd/ca.crt \
+     --name=master \
+     --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key \
+     --data-dir /var/lib/etcd-from-backup \
+     --initial-cluster=master=https://127.0.0.1:2380 \
+     --initial-cluster-token=etcd-cluster-1 \
+     --initial-advertise-peer-urls=https://127.0.0.1:2380 \
+     snapshot restore /tmp/snapshot-pre-boot.db
