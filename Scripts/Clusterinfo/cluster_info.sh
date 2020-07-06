38c00ad99e6f55359a92bc1943dcd04467c245b6
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-07-06 12:09:13 -0500

    Update cluster_info.sh

--- Scripts/Clusterinfo/cluster_info.sh
+++ Scripts/Clusterinfo/cluster_info.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Get Cluster Roles
+# Get Number of Cluster Roles
 printf "==== Get Cluster Roles ==== \n"
   kubectl get clusterroles --no-headers | wc -l
+printf "==== Break for more Info ==== \n"
+printf" \n"
+# Get Number of Cluster Role Bindings
+printf "==== Get Cluster Role Bindings ==== \n"
+kubectl get clusterrolebindings --no-headers | wc -l
 printf "==== Break for more Info ==== \n"
