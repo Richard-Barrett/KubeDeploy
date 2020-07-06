8dee0a9dc48ee35acbfdc383f339658e419d1b6a
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-07-06 09:56:47 -0500

    Update etcd_cert_backup.sh

--- Scripts/etcd_cert_backup.sh
+++ Scripts/etcd_cert_backup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-ETCDCTL_API=3 etcdctl --endpoints=https://[127.0.0.1]:2379 --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key snapshot save /tmp/snapshot-pre-boot.db
+
+# Save Certs in /tmp/snapshot-pre-boot.db as Backup
+ETCDCTL_API=3 etcdctl --endpoints=https://[127.0.0.1]:2379 \
+--cacert=/etc/kubernetes/pki/etcd/ca.crt \
+--cert=/etc/kubernetes/pki/etcd/server.crt \
+--key=/etc/kubernetes/pki/etcd/server.key snapshot save /tmp/snapshot-pre-boot.db
